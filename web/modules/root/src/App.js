665578afc046ce94a5b9dfcc15c329a0ad6c534a
abdulwahab <meabdulwahab23@gmail.com>
2024-05-01 14:43:05 +0500

    version control

---
+++ web/modules/root/src/App.js
@@ -0,0 +1,12 @@
+import { Header, Button } from "@fstr/components"
+
+function App() {
+  return (
+    <div>
+      <Header text="from root" />
+      <Button text="my button" />
+    </div>
+  )
+}
+
+export default App
